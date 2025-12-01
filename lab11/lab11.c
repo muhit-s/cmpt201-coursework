aad133c6de77ed06d435484e8d3980cf1790813a
Muhit Saharaiar <msa363@sfu.ca>
2025-12-01 00:48:56 -0800

    Lab 11 completed

---
+++ lab11/lab11.c
@@ -0,0 +1,130 @@
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define RED "\e[9;31m"
+#define GRN "\e[0;32m"
+#define CRESET "\e[0m"
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+size_t read_all_bytes(const char *filename, void *buffer, size_t buffer_size) {
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    handle_error("Error opening file");
+  }
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  if (file_size > buffer_size) {
+    handle_error("File size is too large");
+  }
+
+  if (fread(buffer, 1, file_size, file) != file_size) {
+    handle_error("Error reading file");
+  }
+
+  fclose(file);
+  return file_size;
+}
+
+void print_file(const char *filename, const char *color) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    handle_error("Error opening file");
+  }
+
+  printf("%s", color);
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    printf("%s", line);
+  }
+  fclose(file);
+  printf(CRESET);
+}
+
+int verify(const char *message_path, const char *sign_path, EVP_PKEY *pubkey);
+
+int main() {
+  // File paths
+  const char *message_files[] = {"message1.txt", "message2.txt",
+                                 "message3.txt"};
+  const char *signature_files[] = {"signature1.sig", "signature2.sig",
+                                   "signature3.sig"};
+
+  // TODO: Load the public key using PEM_read_PUBKEY
+  FILE *file_stream = fopen("public_key.pem", "r");
+  EVP_PKEY *pubkey = PEM_read_PUBKEY(file_stream, NULL, NULL, NULL);
+
+  // Verify each message
+  for (int i = 0; i < 3; i++) {
+    printf("... Verifying message %d ...\n", i + 1);
+    int result = verify(message_files[i], signature_files[i], pubkey);
+
+    if (result < 0) {
+      printf("Unknown authenticity of message %d\n", i + 1);
+      print_file(message_files[i], CRESET);
+    } else if (result == 0) {
+      printf("Do not trust message %d!\n", i + 1);
+      print_file(message_files[i], RED);
+    } else {
+      printf("Message %d is authentic!\n", i + 1);
+      print_file(message_files[i], GRN);
+    }
+  }
+
+  EVP_PKEY_free(pubkey);
+
+  return 0;
+}
+
+/*
+    Verify that the file `message_path` matches the signature `sign_path`
+    using `pubkey`.
+    Returns:
+         1: Message matches signature
+         0: Signature did not verify successfully
+        -1: Message is does not match signature
+*/
+int verify(const char *message_path, const char *sign_path, EVP_PKEY *pubkey) {
+#define MAX_FILE_SIZE 512
+  unsigned char message[MAX_FILE_SIZE];
+  unsigned char signature[MAX_FILE_SIZE];
+
+  FILE *message_stream = fopen(message_path, "r");
+  int message_len =
+      fread(message, sizeof(*message), MAX_FILE_SIZE, message_stream);
+
+  FILE *signature_stream = fopen(sign_path, "r");
+  int signature_len =
+      fread(signature, sizeof(*signature), MAX_FILE_SIZE, signature_stream);
+
+  EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+
+  EVP_DigestVerifyInit(mdctx, NULL, EVP_sha256(), NULL, pubkey);
+  EVP_DigestVerifyUpdate(mdctx, message, message_len);
+  int ret_final = EVP_DigestVerifyFinal(mdctx, signature, signature_len);
+
+  EVP_MD_CTX_free(mdctx);
+
+  if (ret_final == 1)
+    return 1;
+  else if (ret_final == 0)
+    return 0;
+  else
+    return -1;
+  // TODO: Check if the message is authentic using the signature.
+  // Look at: https://wiki.openssl.org/index.php/EVP_Signing_and_Verifying
+
+  return -1;
+}
